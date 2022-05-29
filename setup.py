d88815cb67bae8e0b9b54860d1fa322a82963dcb
“championshuttler” <shivams2799@gmail.com>
2022-05-30 00:51:49 +0530

    added right path

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="It's pip... with git.",
     long_description=long_description,
     url="https://github.com/championshuttler/ECLDocGenerator.git",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(include=['ECLDocGenerator', 'ECLDocGenerator.*']),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
