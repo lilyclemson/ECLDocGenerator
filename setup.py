9c92bd90f476b69be36cfac7ff3c39535939d799
championshuttler <shivams2799@gmail.com>
2022-06-13 22:05:33 +0530

    Fix. gitignore.

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ from setuptools import setup, find_packages
 import os
 
 
-with open("README.rst", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 
 def package_files(directory):
     paths = []
@@ -19,13 +16,11 @@ extra_files = package_files('ecldoc/Templates')
 print(extra_files)
 setup(
     name="ecldoc",
-    version="1.0",
+    version="1.0.2",
     packages=find_packages(),
     install_requires=['Jinja2==2.9.6', 'lxml==3.8.0'],
     package_data={'': extra_files},
     scripts=['bin/ecldoc'],
     url="https://github.com/lilyclemson/ECLDocGenerator",
-    long_description=long_description,
-    long_description_content_type="text/rst",
 )
 
