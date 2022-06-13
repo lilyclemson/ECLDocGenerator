aba4d09c7d152ed15bf8c871e37fe5b3d68b4a52
championshuttler <shivams2799@gmail.com>
2022-06-13 21:19:14 +0530

    Fix. Unable to find ecldoc module due to file structure.

--- setup.py
+++ setup.py
@@ -15,15 +15,15 @@ def package_files(directory):
     return paths
 
 
-extra_files = package_files('src/ecldoc/Templates')
-
+extra_files = package_files('ecldoc/Templates')
+print(extra_files)
 setup(
     name="ecldoc",
     version="1.0",
     packages=find_packages(),
     install_requires=['Jinja2==2.9.6', 'lxml==3.8.0'],
     package_data={'': extra_files},
-    scripts=['src/bin/ecldoc'],
+    scripts=['bin/ecldoc'],
     url="https://github.com/lilyclemson/ECLDocGenerator",
     long_description=long_description,
     long_description_content_type="text/rst",
