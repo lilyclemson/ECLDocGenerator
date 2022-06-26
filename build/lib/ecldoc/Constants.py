794b3de73b29654c08c4b313c6dd35b262098b0e
championshuttler <shivams2799@gmail.com>
2022-06-26 22:27:02 +0530

    Remove unwanted files

--- build/lib/ecldoc/Constants.py
+++
@@ -1,4 +0,0 @@
-import os
-code_dir = os.path.dirname(os.path.realpath(__file__))
-
-TEMPLATE_DIR = os.path.join(code_dir, 'Templates')
--- build/lib/ecldoc/Formats/generators.py
+++
@@ -1,9 +0,0 @@
-from .HTML import genHTML
-from .TXT import genTXT
-from .TEX import genTEX
-
-generators = {
-	'html' : genHTML.GenHTML,
-	'text' : genTXT.GenTXT,
-	'pdf' : genTEX.GenTEX
-}
